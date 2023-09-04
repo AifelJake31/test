68580fc467c911d0128f6403db76269712cf7623
MSI\aifel <aifel.regunay@luckycue.tech>
2023-09-04 14:51:36 +0800

    created index.blade views and learned how to render the data that is coming from the controller

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="continer">
+<div class="container">
     <div class="row">
         <div class="col-6">
-            <div>Account: {{$expenses->account}}</div>
-            <div>Category: {{ $expenses->category}}</div>
-            <div>Amount: {{ $expenses->amount}}</div>
-            <div>Note: {{ $expenses->note}}</div>
+            <div>Account: {{ $expenses->account }}</div>
+            <div>Category: {{ $expenses->category }}</div>
+            <div>Amount: {{ $expenses->amount }}</div>
+            <div>Note: {{ $expenses->note }}</div>
+
+            <a href="{{ route('expenses.create') }}">ADD expenses</a>
         </div>
     </div>
 </div>
+@endsection
